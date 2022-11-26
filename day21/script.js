14bc74451c28ad2f71ebb28c0222f448a1fcba2a
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2022-11-26 12:19:09 +0100

    day21

---
+++ day21/script.js
@@ -0,0 +1,9 @@
+// DOM (Document Object Model)
+const alltitles = document.getElementsByTagName('h1')
+console.log(alltitles)
+console.log(alltitles.length)
+
+for (let i = 0; i < alltitles.length; i++) {
+    console.log(alltitles[i])
+}
+
