709e41a3cfdcf3a829a72ecfc63ae94bb8facc96
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2022-12-07 13:42:45 +0100

    fixes

---
+++ day14/script.js
@@ -0,0 +1,20 @@
+//Link to reading => (https://javascript.info/try-catch)
+
+// Error handling, "try...catch"
+
+// The “try…catch” syntax
+// The try...catch construct has two main blocks: try, and then catch:
+
+// try {
+
+//   // code...
+
+// } catch (err) {
+
+//   // error handling
+
+// }
+
+// How it works
+// 1. First, the code in try {...} is executed.
+// 2. If there were no errors, then catch (err) is ignored: the execution reaches the end of try and goes on, skipping catch.
--- day24/script.js
+++ day24/script.js
@@ -1,19 +1,32 @@
-let select = document.querySelector('select')
+let sb = document.querySelector('#planet')
 
 let mass = document.querySelector('#mass')
 let button = document.querySelector('button')
 
-let opt = document.querySelectorAll
-// console.log(opt)
-for (let i = 0; i < select.length; i++) {
-    let opt = document.createElement('option')
-    opt.textContent = '--select planet--'
-    select.appendChild(opt)
-    if (opt.textContent == 'Mars') {
-        button.addEventListener('click', () => {
-            console.log(333)
-        })
-    }
-}
+let opt = document.querySelector('#first')
 
 
+
+
+const arr = [
+    { planet: "mars", img: "./media/mars.png", gravity: 23 },
+    { planet: "mercury", img: "./media/mercury.png", gravity: 2 },
+    { planet: 'venus', img: './media/venus.png', gravity: 10 },
+    { planet: 'saturn', img: './media/saturn.png', gravity: 10 },
+    { planet: 'jupiter', img: './media/jupiter.png', gravity: 10 },
+    { planet: 'earth', img: './media/earth.png', gravity: 10 },
+    { planet: 'moon', img: './media/moon.png', gravity: 10 },
+    { planet: 'neptune', img: './media/neptune.png', gravity: 10 },
+    { planet: 'pluto', img: './media/pluto.png', gravity: 10 },
+    { planet: 'uranus', img: './media/uranus.png', gravity: 10 }
+]
+
+
+let gravToCal = 0
+button.addEventListener('click', () => {
+    sb.addEventListener('change', (e) => {
+        const result = arr.find((a) => a.planet == e.target.value)
+        console.log(result)
+        gravToCal = result.gravity
+    })
+})
